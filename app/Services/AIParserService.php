405ec1884afe9ba2fd45f14622d36717cd7ed199
Humam Al Amin <alaminhumam11@gmail.com>
2025-06-20 13:05:03 +0700

    first commit

---
+++ app/Services/AIParserService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class AIParserService extends Helper
+{
+    public function parseWithAI(string $text): ?array
+    {
+        $apiKey = env('COHERE_API_KEY'); // simpan di .env
+        $prompt = <<<PROMPT
+            Ubah teks struk belanja berikut menjadi format JSON tanpa penjelasan tambahan apa pun. Cukup hasil JSON-nya saja.
+
+            Contoh format JSON yang diharapkan:
+            {
+            "date": "2025-06-17",
+            "items": [
+                { "name": "Indomie", "qty": 1, "price": 3000, "subtotal": 3000 },
+                { "name": "Aqua", "qty": 2, "price": 2500, "subtotal": 5000 }
+            ],
+            "total": 8000
+            }
+
+            Teks struk:
+            $text
+            PROMPT;
+
+        $response = Http::withToken($apiKey)
+            ->post('https://api.cohere.ai/v1/generate', [
+                'model' => 'command',
+                'prompt' => $prompt,
+                'max_tokens' => 500,
+                'temperature' => 0.2,
+            ]);
+
+        if (! $response->successful()) {
+            return null;
+        }
+
+
+        $raw = $response->json('generations.0.text');
+        $onlyJson = $this->cleanCohereResponse($raw);
+
+        return $onlyJson;
+    }
+}
